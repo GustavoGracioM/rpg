9f8d7fabac96d604ae1bf5b91c3122a1018f83db
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-15 23:51:30 -0300

    feat: create model class

---
+++ backend/src/models/class.model.ts
@@ -0,0 +1,20 @@
+import { Model, Table, Column, DataType, HasMany } from 'sequelize-typescript';
+/* eslint-disable import/no-cycle */
+@Table({
+  tableName: 'class',
+  timestamps: false,
+})
+export default class Class extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'id',
+  })
+    id?: number;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'type',
+  })
+    type?: string;
