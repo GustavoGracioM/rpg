7efcc9486cd214a37f6162c4469e7d2b3c4b1e28
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-15 23:50:22 -0300

    feat: create model user

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import { Model, Table, Column, DataType, HasMany } from 'sequelize-typescript';
+/* eslint-disable import/no-cycle */
+
+@Table({
+  tableName: 'user',
+  timestamps: false,
+})
+export default class User extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'id',
+  })
+    id?: number;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'name',
+  })
+    name?: string;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'email',
+  })
+    email?: string;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'password',
+  })
+    password?: string;
