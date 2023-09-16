34054b7cc1baa9543f2bf6c23121090a641001ce
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-16 00:15:28 -0300

    feat: create middlewares errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import errorHandler from './middlewares/errorHandeler.middlewares';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
 
 export default app;
---
+++ backend/src/middlewares/errors/NotFound.error.ts
@@ -0,0 +1,8 @@
+export default class NotFound extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
