f7113a42a0d94906b1b8ad287cfe7da7d97668ea
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-16 15:01:18 -0300

    feat: create interface expertise

---
+++ backend/src/interfaces/expertise.interface.ts
@@ -0,0 +1,33 @@
+interface IExpertise {
+  id?: number
+  characterId?: number
+  acrobacia: string
+  adestramento: string
+  atletismo: string
+  atualidades: string
+  ciências: string
+  crime: string
+  diplomacia: string
+  enganacao: string
+  fortitude: string
+  furtividade: string
+  iniciativa: string
+  intimidacao: string
+  intuicao: string
+  investigacao: string
+  luta: string
+  medicina: string
+  ocultismo: string
+  percepcao: string
+  pilotagem: string
+  pontaria: string
+  profissao: string
+  reflexos: string
+  religiao: string
+  sobrevivência: string
+  tática: string
+  tecnologia: string
+  vontade: string
+}
+
+export default IExpertise;
