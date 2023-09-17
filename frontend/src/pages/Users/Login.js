162045a41933732ce85887dc1fc664ef5212c7b2
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-17 15:28:02 -0300

    feat: create page login

---
+++ frontend/src/pages/Users/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import api from '../../service/api';
+
+function Login() {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+  const { handleSubmit, register } = useForm();
+
+  const navigateUrl = (url) => {
+    navigate(url);
+  };
+
+  function onSubmit(data) {
+    api
+      .post('/login', data)
+      .then((response) => {
+        localStorage.setItem('token', JSON.stringify(response.data));
+        navigate('/character');
+      })
+      .catch(() => {
+        setErrorMessage('Email ou Senha incorretos');
+      });
+  }
+
+  return (
+    <form onSubmit={ handleSubmit(onSubmit) }>
+      <input { ...register('email') } name="email" />
+      <input { ...register('password') } name="password" />
+      <button type="submit">entrar</button>
+      {errorMessage ? <p>Email ou Senha Invalidos</p> : <br />}
+      <br />
+      <button type="button" onClick={ () => navigateUrl('/register') }>cadastrar</button>
+    </form>
+  );
+}
+
+export default Login;
