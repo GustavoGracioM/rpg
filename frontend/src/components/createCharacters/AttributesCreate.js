8685fa18fbedf08d3f8228d39abeca3e42f5037f
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-17 15:31:53 -0300

    feat: create components AttributesCreate

---
+++ frontend/src/components/createCharacters/AttributesCreate.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { upperCase } from '../../utils/utils';
+
+const optionsSkills = ['agilidade', 'forca', 'intelecto', 'presenca', 'vigor'];
+
+const numbers = {
+  oneLess: -1,
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4 };
+
+function AttributesCreate(prop) {
+  const { register } = prop;
+  return (
+    <>
+      <h1>Atributos</h1>
+      {optionsSkills.map((o) => (
+        <p key={ o } htmlFor={ o }>
+          {upperCase(o)}
+          :
+          {' '}
+
+          <select defaultChecked={ 0 } { ...register(o) } id={ o }>
+            {Object.values(numbers).map((g) => (
+              <option key={ g } value={ g }>{g}</option>
+            ))}
+          </select>
+        </p>
+      ))}
+    </>
+  );
+}
+
+export default AttributesCreate;
