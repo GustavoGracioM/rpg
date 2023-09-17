6de95e7ff6b90dba50badc7639b6e881a7a37941
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-17 15:42:39 -0300

    feat: create route boards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import Characters from './pages/Characters/Characters';
 import Create from './pages/Characters/Create';
 import Character from './pages/Characters/Character';
 import Register from './pages/Users/Register';
+import Boards from './pages/Boards/Boards';
+import Board from './pages/Boards/Board';
+import BoardCharacter from './pages/Boards/BoardCharacters';
+import BoardHistory from './pages/Boards/BoardHistory';
 function App() {
   return (
     <Routes>
@@ -15,6 +19,16 @@ function App() {
       <Route path="character" element={ <Characters /> } />
       <Route path="character/create" element={ <Create /> } />
       <Route path="register" element={ <Register /> } />
+      <Route path="boards" element={ <Boards /> } />
+      <Route path="boards/:id" element={ <Board /> } />
+      <Route
+        path="boards/:boardId/character/:characterId"
+        element={ <BoardCharacter /> }
+      />
+      <Route
+        path="board-history/:boardId"
+        element={ <BoardHistory /> }
+      />
     </Routes>
   );
 }
