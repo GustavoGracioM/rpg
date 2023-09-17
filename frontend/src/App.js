3356fe652a9fb2a39c911f25569484fea74ddee2
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-17 15:29:13 -0300

    feat: create route login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Users/Login';
+import Register from './pages/Users/Register';
 function App() {
   return (
+      <Route path="login" element={ <Login /> } />
+      <Route path="register" element={ <Register /> } />
   );
 }
 
