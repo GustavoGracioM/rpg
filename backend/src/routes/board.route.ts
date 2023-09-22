a4641c6211a65d72c13f5a836bf01b31f9829f67
GustavoGracioM <gustavogracio@hotmail.com>
2023-09-22 14:04:11 -0300

    feat: create route board/user

--- backend/src/routes/board.route.ts
+++ backend/src/routes/board.route.ts
@@ -6,6 +6,7 @@ const route = Router();
 route.get('/', boardController.findAll);
 route.get('/:id', boardController.findById);
 route.post('/', boardController.create);
+route.post('/user', boardController.findByUserId);
 route.delete('/', boardController.delete);
 route.delete('/:id', boardController.deleteById);
 route.put('/:id', boardController.update);
