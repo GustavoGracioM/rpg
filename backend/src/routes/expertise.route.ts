47d87e1a64274f97e1e214ec10e7b213b46971cc
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:04:16 -0300

    fix: remove route unnecessary

--- backend/src/routes/expertise.route.ts
+++ backend/src/routes/expertise.route.ts
@@ -4,10 +4,8 @@ import expertiseController from '../controllers/expertise.controller';
 const route = Router();
 
 route.get('/', expertiseController.findAll);
-route.get('/:id', expertiseController.findByPk);
 route.post('/', expertiseController.create);
 route.delete('/', expertiseController.delete);
 route.delete('/:id', expertiseController.deleteById);
-route.put('/:id', expertiseController.update);
 
 export default route;
