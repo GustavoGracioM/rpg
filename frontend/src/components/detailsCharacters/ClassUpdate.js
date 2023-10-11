2ed68bbb9f4df4a5e5eecfef7ccea84e02b43864
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:15:46 -0300

    fix: correct erros in component class update

--- frontend/src/components/detailsCharacters/ClassUpdate.js
+++ frontend/src/components/detailsCharacters/ClassUpdate.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../service/api';
 
+const template = {
+  class: 'Classe',
+  trail: 'Trilha',
+  origin: 'Origem',
+};
+
 function ClassUpdate(prop) {
-  const { character, type: key, register, releaseButton } = prop;
+  const { character, register, type } = prop;
   const [classSelect, setClassSelect] = useState({});
   const [allClass, setAllClass] = useState([]);
 
   useEffect(() => {
-    api.get('/class').then((response) => {
-      setClassSelect(response.data.find((c) => c.type === character.class));
+    api.get(`/${type}`).then((response) => {
+      setClassSelect(response.data.find((c) => {
+        const result = c.type ? c.type : c.name;
+        return result === character[type];
+      }));
       setAllClass(response.data);
     });
   }, []);
@@ -16,17 +25,28 @@ function ClassUpdate(prop) {
   const setClass = (value) => {
     const resultClass = allClass.find((a) => a.id === parseInt(value, 10));
     setClassSelect(resultClass);
-    releaseButton(key);
   };
   return (
-    <select
-      value={ classSelect.id }
-      { ...register('classId') }
-      onChange={ (e) => setClass(e.target.value) }
-    >
-      {allClass
-        .map((a) => <option key={ a.id } value={ a.id }>{a.type}</option>)}
-    </select>
+    <label htmlFor={ type }>
+      {`${template[type]}`}
+      <select
+        value={ classSelect.id }
+        id={ type }
+        { ...register(`${type}Id`) }
+        onChange={ (e) => setClass(e.target.value) }
+      >
+        {allClass
+          .map((a) => (
+            <option
+              key={ a.id }
+              value={ a.id }
+            >
+              {type === 'class' ? a.type : a.name}
+            </option>
+          ))}
+      </select>
+
+    </label>
   );
 }
 
