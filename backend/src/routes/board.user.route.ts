4892118364d83874ee9f07fcae09528b58b8834e
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:03:23 -0300

    feat: add news routes in board-user

--- backend/src/routes/board.user.route.ts
+++ backend/src/routes/board.user.route.ts
@@ -3,9 +3,11 @@ import boardUserController from '../controllers/board.user.controller';
 
 const route = Router();
 
+route.get('/:boardId', boardUserController.findByBoardId);
 route.get('/', boardUserController.findAll);
 route.post('/', boardUserController.create);
 route.post('/filter', boardUserController.findById);
+route.post('/check-approved', boardUserController.findByCheckApproved);
 route.put('/:id', boardUserController.update);
 route.delete('/all', boardUserController.delete);
 route.delete('/:id', boardUserController.deleteById);
