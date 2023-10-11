810c178b2b719dda2c8fd79b808758cc9b3d4af4
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:21:02 -0300

    feat: add news infos in page board

--- frontend/src/pages/Boards/Board.js
+++ frontend/src/pages/Boards/Board.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import verifyToken from '../../utils/verifyToken';
 import api from '../../service/api';
+import IniviteFriendsBoard from '../../components/boards/InviteFriendsBoard';
+import NavBar from '../../components/navbar/NavBar';
 
 function Board() {
   const params = useParams();
   const navigate = useNavigate();
   const [characters, setCharacters] = useState([]);
-  const [board, setBoard] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [board, setBoard] = useState();
 
   useEffect(() => {
     verifyToken().then((userInfo) => {
@@ -15,16 +18,27 @@ function Board() {
         .then((response) => {
           setCharacters(response.data);
         });
+      api.get(`/board-user/${params.id}`).then((r) => setUsers(r.data));
       api.get(`/boards/${params.id}`).then((r) => setBoard(r.data));
     }).catch((e) => console.log(e));
   }, []);
 
   return (
     <>
-      {board && <h1>{board.name}</h1>}
-      {characters.map((c) => (
+      <NavBar />
+      {board && (
         <>
-          <p key={ c.id }>{c.name}</p>
+          <h1>{board.name}</h1>
+          <h3>{`Dono: ${board.user.name}`}</h3>
+        </>
+      )}
+      <h2>Players</h2>
+      {users.map((u) => <p key={ u.user.id }>{u.user.name}</p>)}
+      <IniviteFriendsBoard />
+      <h2>Seus Personagens</h2>
+      {characters && characters.map((c) => (
+        <>
+          <p key={ `${c.id}` }>{c.name}</p>
           <button
             type="button"
             onClick={ () => navigate(`/boards/${params.id}/character/${c.id}`) }
