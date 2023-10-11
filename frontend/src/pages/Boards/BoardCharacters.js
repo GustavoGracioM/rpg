27e9bf3ee426b96d435f7b171128e5ff4f93505b
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:21:48 -0300

    fix: remove info unnecessary in page board character

--- frontend/src/pages/Boards/BoardCharacters.js
+++ frontend/src/pages/Boards/BoardCharacters.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import Character from '../Characters/Character';
 
 function BoardCharacter() {
-  const params = useParams();
-  console.log(params.characterId, params.boardId);
   return (
-    <>
-      <h1>BoardCharacter</h1>
-      <Character />
-    </>
+    <Character />
   );
 }
 
