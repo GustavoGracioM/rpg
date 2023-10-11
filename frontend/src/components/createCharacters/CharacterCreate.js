11f28086a5fab78af1e9f8bbeed4876bd38e1055
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:11:32 -0300

    fix: remove inputs in character create

--- frontend/src/components/createCharacters/CharacterCreate.js
+++ frontend/src/components/createCharacters/CharacterCreate.js
@@ -1,26 +1,13 @@
 import React from 'react';
 
 function CharacterCreate(prop) {
-  const { characterClass, register } = prop;
+  const { register } = prop;
   return (
     <>
       <h1>Informações Basicas</h1>
       <p>Nome: </p>
       <input name="name" { ...register('name') } />
-      <p>Pontos de Vida: </p>
-      <input name="healthPoints" { ...register('healthPoints') } />
-      <p>Pontos de Esforço: </p>
-      <input name="effortPoints" { ...register('effortPoints') } />
-      <p>Sanidade: </p>
-      <input name="sanity" { ...register('sanity') } />
-      <p htmlFor="class">
-        Classe:
-        <select defaultValue="2" id="class" { ...register('classId') }>
-          {characterClass
-            .map((c) => <option key={ c.id } value={ c.id }>{c.type}</option>)}
-        </select>
-      </p>
-      {' '}
+
     </>
   );
 }
