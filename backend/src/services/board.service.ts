cd7706b8efa6bb2491c71980a9b9d4d4fae825d3
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 00:50:37 -0300

    feat: add junction of tables in return of findbyid in service board

--- backend/src/services/board.service.ts
+++ backend/src/services/board.service.ts
@@ -1,5 +1,15 @@
 import NotFound from '../middlewares/errors/NotFound.error';
 import Board from '../models/board.model';
+import User from '../models/user.model';
+
+const returnFilter = {
+  include: {
+    model: User,
+    as: 'user',
+    attributes: { 
+      exclude: ['password'] },
+  }, 
+};
 
 const boardService = {
   create: async ({ name, userId }: { name: string, userId: number }) => Board
@@ -7,7 +17,7 @@ const boardService = {
 
   findAll: async () => Board.findAll(),
 
-  findById: async (id: number) => Board.findByPk(id),
+  findById: async (id: number) => Board.findByPk(id, returnFilter),
 
   findByUserId: async (userId: number) => Board.findAll({ where: { userId } }),
 
