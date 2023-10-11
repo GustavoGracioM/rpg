7fdb483a7ae2f5332095a3d2d39fbab9b22532c4
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:23:59 -0300

    feat: add news infos in page create character

--- frontend/src/pages/Characters/Create.js
+++ frontend/src/pages/Characters/Create.js
@@ -1,65 +1,90 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import api from '../../service/api';
 import verifyToken from '../../utils/verifyToken';
 import CharacterCreate from '../../components/createCharacters/CharacterCreate';
 import AttributesCreate from '../../components/createCharacters/AttributesCreate';
-import ExpertiseCreate from '../../components/createCharacters/ExpertiseCreate';
+import NavBar from '../../components/navbar/NavBar';
+import OriginsCreate from '../../components/createCharacters/OriginsCreate';
+import TrailCreate from '../../components/createCharacters/TrailCreate';
+import api from '../../service/api';
+import { setClassStates } from '../../utils/utils';
 
-const sapareteInfos = (data, user, skills) => {
+const findCLass = (classId, classInfo, attributes) => {
+  const [presenca, vigor] = [
+    parseInt(attributes.presenca, 10), parseInt(attributes.vigor, 10)];
+  const classSelected = classInfo.find((c) => c.id === parseInt(classId, 10));
+  const result = setClassStates(presenca, vigor)[classSelected.type];
+  return result;
+};
+
+const separateInfos = (data, user) => {
   const character = {
     name: data.name,
     userId: user.id,
-    healthPoints: data.healthPoints,
-    sanity: data.sanity,
-    effortPoints: data.effortPoints };
+  };
   const attributes = {
     agilidade: data.agilidade,
     forca: data.forca,
     intelecto: data.intelecto,
     presenca: data.presenca,
     vigor: data.vigor };
-  const expertise = {};
-  skills.map((s) => data[s.name] && (expertise[s.name] = data[s.name]));
-  return { character, attributes, expertise };
+  return { character, attributes };
+};
+
+const createExpertise = (characterId, expertisesIds, navigate) => {
+  api.post(
+    '/expertise-character',
+    { characterId, expertisesIds },
+  ).then(() => navigate(`/character/${characterId}`));
 };
 
 function CreateCharacter() {
   const navigate = useNavigate();
   const [user, setUser] = useState({});
-  const [characterClass, setCharacterClass] = useState([]);
-  const [skills, setSkills] = useState([]);
+  const [trailId, setTrailId] = useState();
+  const [classId, setClassId] = useState();
+  const [originId, setOriginId] = useState();
+  const [classInfo, setClassInfo] = useState([]);
   const { handleSubmit, register } = useForm();
-
   useEffect(() => {
     verifyToken().then((userInfo) => {
       setUser({ id: userInfo.id });
-      api.get('/class').then((response) => setCharacterClass(response.data));
-      api.get('/skills').then((response) => setSkills(response.data));
-    }).catch(() => navigate('/login'));
+      api.get('/class').then((r) => setClassInfo(r.data));
+    })
+      .catch(() => navigate('/login'));
   }, []);
-
   const onSubmit = async (data) => {
-    const { character, attributes, expertise } = sapareteInfos(data, user, skills);
-    api.post('/character', character).then((response) => {
-      const characterId = response.data.id;
-      const classId = data.classId === '' ? '1' : data.classId;
-      api.post('/class/add', { characterId, classId });
-      api.post('/attributes', { characterId, ...attributes });
-      api.post('/expertise', { characterId, ...expertise }).then(() => {
-        navigate(`/character/${characterId}`);
-      });
-    }).catch((err) => console.log(err));
+    const { character, attributes } = separateInfos(data, user);
+    const statusPoints = findCLass(classId, classInfo, attributes);
+    api.post('/character', { ...character, ...statusPoints, originId, trailId })
+      .then((response) => {
+        const characterId = response.data.id;
+        const [addClass, addAttributes, addOrigins] = [
+          api.post('/class/add', { characterId, classId }),
+          api.post('/attributes', { characterId, ...attributes }),
+          api.get(`/origin/${originId}`)];
+        Promise.all([addClass, addAttributes, addOrigins]).then((r) => {
+          createExpertise(characterId, r[2].data.expertisesIds, navigate);
+        }).catch((err) => console.log(err));
+      }).catch((err) => console.log(err));
   };
-
   return (
-    <form onSubmit={ handleSubmit(onSubmit) }>
-      <CharacterCreate characterClass={ characterClass } register={ register } />
-      <AttributesCreate register={ register } />
-      <ExpertiseCreate skills={ skills } register={ register } />
-      <button type="submit">Entrar</button>
-    </form>
+    <>
+      <NavBar />
+      <form onSubmit={ handleSubmit(onSubmit) }>
+        <CharacterCreate register={ register } />
+        <TrailCreate
+          trailId={ trailId }
+          setTrailId={ setTrailId }
+          classId={ classId }
+          setClassId={ setClassId }
+        />
+        <OriginsCreate originId={ originId } setOriginId={ setOriginId } />
+        <AttributesCreate register={ register } />
+        <button type="submit">Entrar</button>
+      </form>
+    </>
   );
 }
 
