85582773755e7f7ec7b4cd748c74adf74bf4e030
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:10:31 -0300

    fix: correct errors in button rotate

--- frontend/src/components/buttons/ButtonRotate.js
+++ frontend/src/components/buttons/ButtonRotate.js
@@ -1,42 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import rollParser from 'roll-parser';
+// import { useParams } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import api from '../../service/api';
-import bonus, { upperCase } from '../../utils/utils';
+import { upperCase } from '../../utils/utils';
 
 const ten = 10;
+
+const getAttribute = (expertiseAll, attributes, name) => {
+  const findExpertise = expertiseAll.find((e) => e.name === name);
+  const attributeBase = attributes[findExpertise.attributeBase];
+  return { attributeBase, findExpertise };
+};
+
+const defineRoll = (attributeBase) => {
+  if (attributeBase === 0) {
+    const { rolls } = rollParser.parseAndRoll('2d20');
+    const resultRolls = Math.min(...rolls);
+    return { rolls, resultRolls };
+  }
+  const { rolls } = rollParser.parseAndRoll(`${attributeBase}d20`);
+  const resultRolls = Math.max(...rolls);
+  return { rolls, resultRolls };
+};
+
+const defineMessage = ({ name, findExpertise, resultRolls, bonus, amount, rolls }) => {
+  const returnMessage = `${upperCase(name)} + ${upperCase(findExpertise.attributeBase)}:
+  ${resultRolls} + ${bonus} = ${amount}
+  Valor dos Dados:${rolls.map((r) => ` ${r}`)}`;
+  return returnMessage;
+};
 
 function ButtonRotate(prop) {
   const { expertise } = prop;
   const params = useParams();
-  const { grade, key, attributes } = expertise;
+  const { bonus, name, attributes } = expertise;
   const [expertiseAll, setExpertiseAll] = useState([]);
 
   useEffect(() => {
-    api.get('/skills').then((r) => setExpertiseAll(r.data));
+    api.get('/expertise').then((r) => setExpertiseAll(r.data));
   }, []);
 
   const validateBoard = ({ amount, returnMessage }) => {
     const { characterId, boardId } = params;
     if (!characterId && !boardId) return console.log(returnMessage);
-    console.log(amount, characterId);
     api.post('/history-roll', {
       amount, returnMessage, characterId, boardId, type: 'expertise' })
       .then((r) => console.log(r.data));
   };
 
   const rotate = () => {
-    const findExpertise = expertiseAll.find((e) => e.name === key);
-    const attributeBase = attributes[findExpertise.attributeBase] + 1;
-    const { rolls } = rollParser.parseAndRoll(`${attributeBase}d20`);
-    const maxRoll = Math.max(...rolls);
-    const addBonus = bonus(grade) === '0' ? '+0' : bonus(grade);
-    const bonusSum = parseInt(addBonus.split('+')[1], ten);
-    const amount = maxRoll + bonusSum;
-    const returnMessage = `${upperCase(key)} + ${upperCase(findExpertise.attributeBase)}:
-${maxRoll} + ${bonusSum} = ${amount}
-Valor dos Dados: ${rolls}`;
-    validateBoard({ amount: maxRoll + bonusSum, returnMessage });
+    const { attributeBase, findExpertise } = getAttribute(expertiseAll, attributes, name);
+    const { rolls, resultRolls } = defineRoll(attributeBase);
+    const defineBonus = bonus === '0' ? 0 : parseInt(bonus.split('+')[1], ten);
+    const amount = resultRolls + defineBonus;
+    const returnMessage = defineMessage({ name,
+      findExpertise,
+      resultRolls,
+      bonus: defineBonus,
+      amount,
+      rolls,
+    });
+    validateBoard({ amount, returnMessage });
   };
 
   return (
