47083270c053a1ab91584085cbeb4713eb0f858d
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:00:01 -0300

    fix: alter skills to expertise in controllers

--- backend/src/controllers/expertise.controller.ts
+++ backend/src/controllers/expertise.controller.ts
@@ -1,38 +1,18 @@
 import { Request, Response } from 'express';
 import expertiseService from '../services/expertise.service';
-import validateAttributes from '../utils/validateObject';
-import IExpertise from '../interfaces/expertise.interface';
+import NotFound from '../middlewares/errors/NotFound.error';
 
 const expertiseController = {
-  create: async (req: Request, res: Response) => {
-    const { characterId } = req.body;
-    delete req.body.characterId;
-    const expertise = req.body as IExpertise;
-    validateAttributes(expertise);
-    const result = await expertiseService
-      .create(expertise, characterId);
-    res.status(201).json(result);
+  create: async (_req: Request, res: Response) => {
+    const result = await expertiseService.create();
+    if (!result) throw new NotFound('expertise already created');
+    res.status(201).json({ message: 'create' });
   },
 
   findAll: async (_req: Request, res: Response) => {
     const result = await expertiseService.findAll();
     res.status(200).json(result);
   },
-
-  findByPk: async (req:Request, res: Response) => {
-    const id = parseInt(req.params.id, 10);
-    const result = await expertiseService.findByPk(id);
-    res.status(200).json(result);
-  },
-
-  update: async (req: Request, res: Response) => { 
-    const id = parseInt(req.params.id, 10);
-    const expertise = req.body as IExpertise;
-    // validateAttributes(expertise);
-    await expertiseService
-      .update(expertise, id);
-    res.status(201).json({ message: 'updated' });
-  }, 
 
   delete: async (_req: Request, res: Response) => {
     await expertiseService.delete();
