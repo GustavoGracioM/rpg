ff3db69165876d0220f246de7e087402df61d97e
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 00:57:32 -0300

    feat: add find by check approved and find by board id in controller board-user

--- backend/src/controllers/board.user.controller.ts
+++ backend/src/controllers/board.user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import boardUserService from '../services/board.user.service';
 import IBoardUser from '../interfaces/board.user.interface';
+import friendsListService from '../services/friends.list.service';
 
 const boardUserController = {
   create: async (req: Request, res: Response) => {
@@ -18,6 +19,19 @@ const boardUserController = {
   findById: async (req: Request, res: Response) => {
     const { userId, status }: { userId:number, status:string } = req.body;
     const result = await boardUserService.findById(userId, status);
+    res.status(200).json(result);
+  },
+
+  findByCheckApproved: async (req: Request, res: Response) => {
+    const { boardId, userId }: { boardId:number, userId:number } = req.body;
+    const myFriends = await friendsListService.findMyFriends(userId);
+    const result = await boardUserService.findByCheckApproved(boardId, myFriends);
+    res.status(200).json(result);
+  },
+
+  findByBoardId: async (req: Request, res: Response) => {
+    const boardId = parseInt(req.params.boardId, 10);
+    const result = await boardUserService.findByBoardId(boardId);
     res.status(200).json(result);
   },
 
