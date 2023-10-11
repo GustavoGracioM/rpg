57e9f59fd1c171dfc2e2fe7f028c33c29768f2ab
GustavoGracioM <gustavogracio@hotmail.com>
2023-10-11 01:06:15 -0300

    feat: add news routes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,18 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import errorHandler from './middlewares/errorHandeler.middlewares';
-import characterRoute from './routes/character.route';
-import attributesRoute from './routes/attributes.route';
-import classRoute from './routes/class.route';
-import skillsRoute from './routes/skills.route';
-import inventoryRoute from './routes/inventory.route';
-import expertiseRoute from './routes/expertise.route';
-import userRoute from './routes/user.route';
-import attacksRoute from './routes/attacks.route';
-import historyRollRoute from './routes/history.roll.route';
-import boardRoute from './routes/board.route';
-import boardUserRoute from './routes/board.user.route';
-import friendsListRoute from './routes/friends.list.route';
+import { attacksRoute, attributesRoute, boardRoute, boardUserRoute, 
+  characterRoute, classRoute, expertiseRoute, friendsListRoute, 
+  historyRollRoute, inventoryRoute, userRoute, ritualRoute,
+  originsRoute, trailRoute, trainingRoute, expertiseCharacterRoute } from './routes';
 
 const app = express();
 app.use(express.json());
@@ -22,7 +14,6 @@ app.use(cors());
 app.use('/character', characterRoute);
 app.use('/attributes', attributesRoute);
 app.use('/class', classRoute);
-app.use('/skills', skillsRoute);
 app.use('/inventory', inventoryRoute);
 app.use('/expertise', expertiseRoute);
 app.use('/attacks', attacksRoute);
@@ -30,6 +21,11 @@ app.use('/history-roll', historyRollRoute);
 app.use('/boards', boardRoute);
 app.use('/board-user', boardUserRoute);
 app.use('/friends-list', friendsListRoute);
+app.use('/ritual', ritualRoute);
+app.use('/origin', originsRoute);
+app.use('/trail', trailRoute);
+app.use('/training', trainingRoute);
+app.use('/expertise-character', expertiseCharacterRoute);
 app.use(userRoute);
 
 app.use(errorHandler);
