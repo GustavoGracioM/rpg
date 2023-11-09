d561847a0a2b69ec98e925f1cda46556640777b1
GustavoGracioM <gustavogracio@hotmail.com>
2023-11-09 15:19:04 -0300

    feat: create theme light

---
+++ frontend/src/shared/themes/Light.js
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material';
+import { yellow } from '@mui/material/colors';
+
+const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[500],
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: yellow[500],
+      dark: yellow[400],
+      light: yellow[300],
+      contrastText: '#ffffff',
+    },
+    background: {
+      default: '#f7f6f3',
+      paper: '#ffffff',
+    },
+  },
+});
+
+export default LightTheme;
