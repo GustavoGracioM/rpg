dcb499495807ae4e036a980e0c21b9b01c0e66ca
GustavoGracioM <gustavogracio@hotmail.com>
2024-03-21 22:15:38 -0300

    feat: add style in page register

--- frontend/src/pages/Users/Register.js
+++ frontend/src/pages/Users/Register.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import api from '../../service/api';
 import DarkThema from '../../components/darkThema/DarkThema';
+import { buttonSingIn, defaultTextField } from './components/componentsForm';
 
-function Register() {
+const boxConfig = {
+  marginTop: 8,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+export default function SignIn() {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
   const { handleSubmit, register } = useForm();
-
   function onSubmit(data) {
     api
       .post('/register', data)
@@ -22,16 +34,26 @@ function Register() {
   }
   return (
     <DarkThema>
-      <form onSubmit={ handleSubmit(onSubmit) }>
-        <input { ...register('name') } name="name" />
-        <input { ...register('email') } name="email" />
-        <input { ...register('password') } name="password" />
-        <button type="submit">cadastrar</button>
-        {errorMessage ? <p>{errorMessage}</p> : <br />}
-        <br />
-      </form>
+      <Container component="main" maxWidth="xs">
+        <Box sx={ boxConfig }>
+          <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">Sign up</Typography>
+          <Box
+            component="form"
+            onSubmit={ handleSubmit(onSubmit) }
+            noValidate
+            sx={ { mt: 1 } }
+          >
+            {defaultTextField('name', 'User Name', register)}
+            {defaultTextField('email', 'Email Address', register)}
+            {defaultTextField('password', 'Password', register)}
+            {buttonSingIn('Sign Up')}
+            {errorMessage ? <p>Email ou Senha Invalidos</p> : <br />}
+          </Box>
+        </Box>
+      </Container>
     </DarkThema>
   );
 }
-
-export default Register;
